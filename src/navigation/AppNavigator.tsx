d6837077ca9ee9d52fc9b081fa12493f49bdf304
Carlos Cesar Feitosa dos Santos <ccesar.santos@dataprev.gov.br>
2026-03-03 12:16:22 -0300

    feat: linking configuration

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,11 +2,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StorefrontScreen from "../screens/StorefrontScreen";
 import AgeCheckScreen from "../screens/AgeCheckScreen";
 import SuccessScreen from "../screens/SuccessScreen";
+import UnderageScreen from "../screens/UnderageScreen";
 
 export type RootStackParamList = {
   Storefront: undefined;
   AgeCheck: undefined;
-  Success: { requestId: string };
+  Success: { requestId: string; isOver18: boolean };
+  Underage: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -17,6 +19,7 @@ export default function AppNavigator() {
       <Stack.Screen name="Storefront" component={StorefrontScreen} />
       <Stack.Screen name="AgeCheck" component={AgeCheckScreen} />
       <Stack.Screen name="Success" component={SuccessScreen} />
+      <Stack.Screen name="Underage" component={UnderageScreen} />
     </Stack.Navigator>
   );
 }
